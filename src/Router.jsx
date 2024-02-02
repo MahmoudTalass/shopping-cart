3b4af79bd390f630c3a0e88f3f4bed0537359bf9
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-01 21:28:37 -0500

    Install react, react router, and react testing library dependencies

---
+++ src/Router.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function Router() {
+   const router = createBrowserRouter([]);
+
+   return <RouterProvider router={router} />;
+}
