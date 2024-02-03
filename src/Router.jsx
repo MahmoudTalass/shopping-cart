8c6e0422acad60c8c9f261dd0a45387423692db6
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-03 00:13:57 -0500

    Create routes for components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,29 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import HomePage from "./Components/Homepage/HomePage";
+import ShopPage from "./Components/ShopPage/ShopPage";
+import CartPage from "./Components/CartPage/CartPage";
+import Error from "./Components/Error/Error";
 
 export default function Router() {
-   const router = createBrowserRouter([]);
+   const router = createBrowserRouter([
+      {
+         path: "/",
+         element: <App />,
+         errorElement: <Error />,
+         children: [
+            { path: "home", element: <HomePage /> },
+            {
+               path: "shop",
+               element: <ShopPage />,
+            },
+            {
+               path: "cart",
+               element: <CartPage />,
+            },
+         ],
+      },
+   ]);
 
    return <RouterProvider router={router} />;
 }
