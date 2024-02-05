97fe4f09837ef20d7296a81b7e367f98076c3c1a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-05 17:21:35 -0500

    Create and style basic error page
    This page is shown when user enters a non existing route. Contains a
    link to the home route/page.

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,1 +1,11 @@
-export default function Error() {}
+import { Link } from "react-router-dom";
+import styles from "./Error.module.css";
+
+export default function Error() {
+   return (
+      <div className={styles.errorContainer}>
+         <h1>Page not found!</h1>
+         <Link to="/">Return home</Link>
+      </div>
+   );
+}
