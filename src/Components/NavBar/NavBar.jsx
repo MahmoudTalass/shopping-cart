d3e23d9fba1da87b361f837764a31bfc18f41f81
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-05 17:06:29 -0500

    Create a navigation bar with links and style it

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,1 +1,15 @@
-export default function NavBar() {}
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+   return (
+      <nav className={styles.nav}>
+         <Link to="/" className={styles.navLink}>
+            Home
+         </Link>
+         <Link to="/shop" className={styles.navLink}>
+            Shop
+         </Link>
+      </nav>
+   );
+}
