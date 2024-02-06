5afef0537b3255968b0cae3742b4c5a1d75201be
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-05 19:10:47 -0500

    Show better error message to user when error happens

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import styles from "./Error.module.css";
 
 export default function Error() {
+   const error = useRouteError();
+
    return (
       <div className={styles.errorContainer}>
-         <h1>Page not found!</h1>
+         <h1>Oops!</h1>
+         <p>Sorry, an unexpected error has occured</p>
+         <p>{error.statusText || error.message}</p>
          <Link to="/">Return home</Link>
       </div>
    );
