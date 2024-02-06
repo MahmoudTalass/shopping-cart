91b049496b89ba30d4543ec321877dfc02e7f670
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-05 19:11:31 -0500

    Fetch products data inside of useEffect
    Show loading screen when data is loading and an error when an error
    happens

--- src/Components/ShopPage/ShopPage.jsx
+++ src/Components/ShopPage/ShopPage.jsx
@@ -1,5 +1,59 @@
 import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function ShopPage() {
+   const [products, setProducts] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+   useEffect(() => {
+      async function fetchProducts() {
+         try {
+            const response = await fetch(
+               "https://fakestoreapi.com/products/category/jewelery"
+            );
+
+            if (!response.ok) {
+               throw new Error(response.statusText);
+            }
+            const jsonData = await response.json();
+
+            if (!ignore) {
+               setProducts(jsonData);
+            }
+         } catch (error) {
+            setProducts(null);
+            setError(error);
+         } finally {
+            setLoading(false);
+         }
+      }
+
+      let ignore = false;
+      fetchProducts();
+      return () => {
+         ignore = true;
+      };
+   }, []);
+
    const { handleBagChanges } = useOutletContext();
+
+   if (error) {
+      return (
+         <div>
+            <h1>Network error</h1>
+            <p>{error.message}</p>
+         </div>
+      );
+   }
+
+   if (loading) return <p>Loading...</p>;
+
+   return (
+      <div>
+         {products.map((product) => {
+            return product.title;
+         })}
+      </div>
+   );
 }
