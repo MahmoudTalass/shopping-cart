7fe8c7a13e08ff85b9d3a9faab9c89b515e1e7b4
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-10 19:21:52 -0500

    Add product count to let user select how much of a product they want
    Style the buttons and the containers associated with counting.

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,13 +1,56 @@
 import PropTypes from "prop-types";
 import styles from "./ProductCard.module.css";
+import { useState } from "react";
 
 function ProductCard({ title, image, description, price }) {
+   const [productCount, setProductCount] = useState(0);
+
+   function handleIncrementProductCount() {
+      if (productCount != 100) {
+         setProductCount((count) => count + 1);
+      }
+   }
+
+   function handleDecrementProductCount() {
+      if (productCount != 0) {
+         setProductCount((count) => count - 1);
+      }
+   }
+
    return (
       <div className={styles.productCard}>
          <img className={styles.productImage} src={image} alt={title} />
          <p className={styles.title}>{title}</p>
          <p className={styles.price}>${price}</p>
          <p className={styles.description}>{description}</p>
+         <div className={styles.addingProductToBagContainer}>
+            <div className={styles.productCountAdjustmentsContainer}>
+               <button
+                  className={styles.incrementDecrementBtn}
+                  aria-label="decrement"
+                  onClick={handleDecrementProductCount}
+               >
+                  -
+               </button>{" "}
+               <input
+                  className={styles.productCountInput}
+                  type="number"
+                  max={100}
+                  value={productCount}
+                  onChange={(e) => {
+                     setProductCount(e.target.value);
+                  }}
+               />
+               <button
+                  className={styles.incrementDecrementBtn}
+                  aria-label="increment"
+                  onClick={handleIncrementProductCount}
+               >
+                  +
+               </button>
+            </div>
+            <button>Add to bag</button>
+         </div>
       </div>
    );
 }
