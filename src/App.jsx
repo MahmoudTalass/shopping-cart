0d0cf114a9a1acfd4d5f62ba639ce1c1b64253cc
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-02-17 17:42:02 -0500

    Set up structure for bag state and a function to set that state
    The bag state and handler function for it are passed as context to be
    received as outlet context and used in other components.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,31 @@ import "./pageLayout.css";
 import { useState } from "react";
 
 export default function App() {
-   const [bag, setBag] = useState([]);
+   const [bag, setBag] = useState({});
 
    function handleBagChanges(item) {
-      setBag([...bag, item]);
+      if (item.id in bag) {
+         setBag((prev) => ({
+            ...prev,
+            [item.id]: { ...prev[item.id], quantity: prev[item.id].quantity + 1 },
+         }));
+      } else {
+         setBag({
+            ...bag,
+            [item.id]: { ...item },
+         });
+      }
+   }
+
+   let itemsCount = 0;
+
+   for (let item in bag) {
+      itemsCount += item.quantity;
    }
 
    return (
       <>
-         <Header />
+         <Header itemsCount={itemsCount} />
          <Outlet context={(bag, handleBagChanges)} />
          <Footer />
       </>
