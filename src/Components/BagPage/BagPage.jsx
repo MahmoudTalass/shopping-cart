f33203a1f360ced82abf35c39d04dd38adbd0a1e
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-04 09:43:52 -0500

    Create and style a bag page that has the products in user's bag

--- src/Components/BagPage/BagPage.jsx
+++ src/Components/BagPage/BagPage.jsx
@@ -1,1 +1,15 @@
-export default function BagPage() {}
+import { useOutletContext } from "react-router-dom";
+import ProductCardInBag from "../ProductCardInBag/ProductCardInBag";
+import style from "./BagPage.module.css";
+
+export default function BagPage() {
+   const { bag } = useOutletContext();
+
+   return (
+      <main className={style.bagContainer}>
+         {Object.keys(bag).map((productId) => {
+            return <ProductCardInBag key={productId} {...bag[productId]} />;
+         })}
+      </main>
+   );
+}
