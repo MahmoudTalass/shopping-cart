460950493c8704fc73bdf9c5694add85b503c4da
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-14 18:07:21 -0400

    Create checkout btn and bag total price and success msg after checkout
    This success message appears for 6 seconds and then is dismounted

--- src/Components/BagPage/BagPage.jsx
+++ src/Components/BagPage/BagPage.jsx
@@ -2,14 +2,25 @@ import { useOutletContext } from "react-router-dom";
 import ProductCardInBag from "../ProductCardInBag/ProductCardInBag";
 import style from "./BagPage.module.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function BagPage() {
-   const { bag } = useOutletContext();
+   const { bag, clearBag } = useOutletContext();
+   const [showAlert, setShowAlert] = useState(false);
+
+   useEffect(() => {
+      const timeoutId = setTimeout(() => {
+         setShowAlert(false);
+      }, 6000);
+      return () => clearTimeout(timeoutId);
+   }, [showAlert]);
 
    const isBagEmpty = Object.keys(bag).length === 0;
+   let total = 0;
 
    return (
       <main className={style.bagContainer}>
+         {showAlert && <p className={style.purchaseSuccessAlert}>Success</p>}
          <div className={style.bagProductCardsContainer}>
             {isBagEmpty && (
                <p>
@@ -17,9 +28,23 @@ export default function BagPage() {
                </p>
             )}
             {Object.keys(bag).map((productId) => {
+               total += bag[productId].quantity * bag[productId].price;
                return <ProductCardInBag key={productId} {...bag[productId]} />;
             })}
          </div>
+         {!isBagEmpty && (
+            <div className={style.checkoutContainer}>
+               <p>Total: ${total}</p>
+               <button
+                  onClick={() => {
+                     setShowAlert(true);
+                     clearBag();
+                  }}
+               >
+                  Checkout
+               </button>
+            </div>
+         )}
       </main>
    );
 }
