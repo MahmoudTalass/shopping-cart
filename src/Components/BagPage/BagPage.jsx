521f75358d119a8e9d8e4bd551f321a7aacc86fe
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-14 17:15:06 -0400

    If bag is empty let user know and give them link to shop route

--- src/Components/BagPage/BagPage.jsx
+++ src/Components/BagPage/BagPage.jsx
@@ -1,13 +1,21 @@
 import { useOutletContext } from "react-router-dom";
 import ProductCardInBag from "../ProductCardInBag/ProductCardInBag";
 import style from "./BagPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function BagPage() {
    const { bag } = useOutletContext();
 
+   const isBagEmpty = Object.keys(bag).length === 0;
+
    return (
       <main className={style.bagContainer}>
          <div className={style.bagProductCardsContainer}>
+            {isBagEmpty && (
+               <p>
+                  Your bag is empty! <Link to="/shop">Shop here</Link>
+               </p>
+            )}
             {Object.keys(bag).map((productId) => {
                return <ProductCardInBag key={productId} {...bag[productId]} />;
             })}
