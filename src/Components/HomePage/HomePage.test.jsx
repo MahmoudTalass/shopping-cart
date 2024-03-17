a01aa4593d2d04c96bdbe2ac233780e072b69fb4
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-17 16:53:23 -0400

    Test existance of shop button and front page image
    Wrap the HomePage component in BrowserRouter to contexialize the test
    and for it to work with the react-dom-router.

--- src/Components/HomePage/HomePage.test.jsx
+++ src/Components/HomePage/HomePage.test.jsx
@@ -1,10 +1,25 @@
+/* eslint-disable no-undef */
 import { render, screen } from "@testing-library/react";
 import HomePage from "./HomePage";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Home page tests", () => {
-   it("Link renders", () => {
-      render(<HomePage />);
+   it("Ship Link renders", () => {
+      render(
+         <BrowserRouter>
+            <HomePage />
+         </BrowserRouter>
+      );
 
       expect(screen.queryByRole("link").textContent).toBe("Shop");
+   });
+   it("Ship Link renders", () => {
+      render(
+         <BrowserRouter>
+            <HomePage />
+         </BrowserRouter>
+      );
+
+      expect(screen.queryByAltText("jewelry")).not.toBe(null);
    });
 });
