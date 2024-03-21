a643212f816384f7a832d2528a6b7d36d4bc7ca1
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-20 21:49:26 -0400

    Refactor the way useOutletContext was being mocked
    Make it so useOutletContext is mocked differently for different tests
    that test out different things and mock the product cards to simplify
    tests

--- src/Components/BagPage/BagPage.test.jsx
+++ src/Components/BagPage/BagPage.test.jsx
@@ -1,71 +1,98 @@
 /* eslint-disable no-undef */
 import BagPage from "./BagPage";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
-const mockBag = {
-   1: {
-      image: "https://placeholder.pics/svg/300",
-      title: "product#1",
-      description: "product#1 desc",
-      price: 3,
-      id: 1,
-      quantity: 2,
-   },
-   2: {
-      image: "https://placeholder.pics/svg/300",
-      title: "product#2",
-      description: "product#2 desc",
-      price: 50,
-      id: 2,
-      quantity: 3,
-   },
-   3: {
-      image: "https://placeholder.pics/svg/300",
-      title: "product#3",
-      description: "product#3 desc",
-      price: 10,
-      id: 3,
-      quantity: 1,
-   },
-};
+vi.mock("react-router-dom");
+vi.mock("../ProductCardInBag/ProductCardInBag", () => ({
+   default: ({ image, title, price, quantity }) => (
+      <article>
+         <img src={image} alt={title} />
+         <p>{title}</p>
+         <p>{price}</p>
+         <p>{quantity}</p>
+      </article>
+   ),
+}));
 
-vi.mock("react-router-dom", async (importActual) => {
-   const actual = await importActual();
-   return {
-      ...actual,
-      useOutletContext: () => {
+describe("BagPage component tests", () => {
+   it("render bag items", () => {
+      const mockBag = {
+         1: {
+            image: "https://placeholder.pics/svg/300",
+            title: "product#1",
+            description: "product#1 desc",
+            price: 3,
+            id: 1,
+            quantity: 2,
+         },
+         2: {
+            image: "https://placeholder.pics/svg/300",
+            title: "product#2",
+            description: "product#2 desc",
+            price: 50,
+            id: 2,
+            quantity: 3,
+         },
+         3: {
+            image: "https://placeholder.pics/svg/300",
+            title: "product#3",
+            description: "product#3 desc",
+            price: 10,
+            id: 3,
+            quantity: 1,
+         },
+      };
+      useOutletContext.mockImplementationOnce(() => {
          return {
             clearBag: () => "cleared",
             bag: mockBag,
          };
-      },
-   };
-});
+      });
 
-let total = Object.keys(mockBag).reduce((accumulator, key) => {
-   return accumulator + mockBag[key].quantity * mockBag[key].price;
-}, 0);
-
-describe("BagPage component tests", () => {
-   it("render bag items", () => {
-      render(
-         <MemoryRouter>
-            <BagPage />
-         </MemoryRouter>
-      );
+      render(<BagPage />);
 
       expect(screen.getAllByRole("article").length).toBe(3);
    });
 
    it("display total and checkout button", () => {
-      render(
-         <MemoryRouter>
-            <BagPage />
-         </MemoryRouter>
-      );
+      const mockBag = {
+         3: {
+            image: "https://placeholder.pics/svg/300",
+            title: "product#3",
+            description: "product#3 desc",
+            price: 10,
+            id: 3,
+            quantity: 1,
+         },
+      };
+
+      useOutletContext.mockImplementationOnce(() => {
+         return {
+            clearBag: () => "cleared",
+            bag: mockBag,
+         };
+      });
+
+      render(<BagPage />);
+
+      const total = Object.keys(mockBag).reduce((accumulator, key) => {
+         return accumulator + mockBag[key].quantity * mockBag[key].price;
+      }, 0);
 
       expect(screen.getByText(`Total: $${total}`)).toBeInTheDocument();
       expect(screen.getByRole("button", { name: /checkout/i })).toBeInTheDocument();
+   });
+
+   it("render empty bag", () => {
+      useOutletContext.mockImplementationOnce(() => {
+         return {
+            clearBag: () => "cleared",
+            bag: {},
+         };
+      });
+      render(<BagPage />);
+
+      expect(screen.getByText(/your bag is empty!/i)).toBeInTheDocument();
    });
 });
