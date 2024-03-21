1ef278dcfec18257b32e74a9b45fc5d1e2394faf
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-03-20 21:59:29 -0400

    Correct import

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import HomePage from "./Components/Homepage/HomePage";
+import HomePage from "./Components/HomePage/HomePage";
 import ShopPage from "./Components/ShopPage/ShopPage";
 import BagPage from "./Components/BagPage/BagPage";
 import Error from "./Components/Error/Error";
